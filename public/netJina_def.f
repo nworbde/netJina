850971dc7d93bbaa49e26ff7336bc2ea2e6bf61d
Edward Brown <ebrown@pa.msu.edu>
2014-05-08 08:00:39 -0400

    added table of elements
    full table of elements, plus synonyms for hydrogen and Al isomers, are
    now in netJina_def

--- public/netJina_def.f
+++ public/netJina_def.f
@@ -18,12 +18,48 @@ module netJina_def
 	integer, parameter :: max_species_per_reaction=6
 	integer, parameter :: ncoefficients=7
 	integer, parameter :: nchapters=11
-	integer, parameter :: ninverse_coeff = 2
-	integer, parameter :: max_ec_rates = 8
 	integer, parameter :: max_terms_per_rate = 20
     ! we keep track of reactions using a unique "handle" generated from the 
-    ! isotopie names
+    ! isotope names
 	integer, parameter :: max_id_length = 42
+    ! max. Z in the nucchem database
+	integer, parameter :: max_element_z = 112
+    
+    ! table of elements; note that hydrogen is referred to as 'p' in the table 
+    ! and neutrons are referred to as 'n'
+	character(len=iso_name_length), dimension(0:max_element_z) ::  &
+	& element_name = [character(len=iso_name_length) :: & 
+	& 'neut','h','he','li','be','b','c','n','o','f','ne', 'na','mg','al','si', &
+	& 'p', 's', 'cl', 'ar', 'k', 'ca', 'sc', 'ti', 'v','cr','mn', 'fe', 'co', &
+	& 'ni','cu','zn','ga','ge','as','se','br','kr','rb','sr','y','zr', &
+	& 'nb','mo','tc','ru','rh','pd','ag','cd','in','sn','sb','te','i','xe', &
+	& 'cs','ba','la','ce','pr','nd','pm','sm','eu','gd','tb','dy','ho','er', &
+	& 'tm','yb', 'lu','hf','ta','w','re','os','ir','pt','au','hg', &
+	& 'tl','pb','bi','po','at','rn','fr','ra','ac','th','pa','u','np','pu', &
+	& 'am','cm','bk','cf','es','fm','md','no','lr','rf','db','sg','bh','hs', &
+	& 'mt','ds','rg','cn' ]
+    
+	! synonyms of hydrogen isotopes, index by mass number
+	character(len=iso_name_length), dimension(3) ::  &
+	& h_isotopes = [character(len=iso_name_length) :: 'p','d','t']
+
+	! aluminum-26 isomers
+	character(len=iso_name_length), dimension(2:3) ::  &
+	& al_isomers = [character(len=iso_name_length) :: 'al-6','al*6']
+    
+    ! flags for reaction channels, bdat-style
+	integer, parameter :: i_pg = 1
+	integer, parameter :: i_ag = i_pg+1
+	integer, parameter :: i_ap = i_ag+1
+	integer, parameter :: i_ng = i_ap+1
+	integer, parameter :: i_np = i_ng+1
+	integer, parameter :: i_gp = i_np+1
+	integer, parameter :: i_an = i_gp+1
+	integer, parameter :: i_ga = i_an+1
+	integer, parameter :: i_pa = i_ga+1
+	integer, parameter :: i_gn = i_pa+1
+	integer, parameter :: i_pn = i_gn+1
+    integer, parameter :: N_bdat_channels = i_pn
     
 	! flags for reaction types: r_<number in>_<number out> = chapter id
 	integer, parameter :: &
