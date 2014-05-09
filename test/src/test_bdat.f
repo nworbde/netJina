69d08431821212fd8cd2528c9d1a1456cd91393c
Edward Brown <ebrown@pa.msu.edu>
2014-05-08 22:29:54 -0400

    tested handle generation
    and it works. Now to return the rate coeffiencts.

--- test/src/test_bdat.f
+++ test/src/test_bdat.f
@@ -8,7 +8,7 @@ program test_bdat
     type(reaclib_data) :: reaclib
     type(nuclib_data) :: nuclib
     integer :: ierr
-    integer :: i,j
+    integer :: i,j,i_rate
     type(integer_dict), pointer :: rates_dict=>null(), nuclide_dict=>null()
     character(len=max_id_length),dimension(N_bdat_channels) :: handles
     character(len=iso_name_length) :: iso
@@ -20,5 +20,14 @@ program test_bdat
     end if
     iso = 'fe56'
     call make_channel_handles(iso,nuclib,nuclide_dict,handles,ierr)
-    write (output_unit,'(i3,tr2,a)') (i,trim(handles(i)), i=1,N_bdat_channels)
+    do i = 1, N_bdat_channels
+        call integer_dict_lookup(rates_dict,trim(handles(i)),i_rate,ierr)
+        if (ierr == 0) then
+            write (output_unit,*) i,trim(handles(i)), &
+            & (trim(reaclib% species(j,i_rate)),j=1,4)
+        else
+            write (output_unit,*) i,trim(handles(i)), 'rate not found'
+        end if
+    end do
+
 end program test_bdat
